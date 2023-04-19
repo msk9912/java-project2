ae112dabf6af3c2aec1329aa85fab6e213f3ecfa
Ms Kim <myungseokim9912@gmail.com>
2023-04-19 15:08:58 +0900

    feat: 문자열 받기 - 문자열 받기

---
+++ src/main/java/com/example/javaproject2/week1/BufferedReaderExTest.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.week1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BufferedReaderExTest {
+
+        public void readALine() {
+            InputStreamReader is = new InputStreamReader(System.in);
+            BufferedReader br = new BufferedReader(is); // 카트
+        }
+}
