ffdeb3c763b63d5b472567fa2cf541ddc84fdcda
Ms Kim <myungseokim9912@gmail.com>
2023-04-19 14:23:39 +0900

    feat: 마지막 크롤 시간 작업1 - CrawlDate 도메인 추가

---
+++ src/main/java/com/example/javaproject2/week1/JavaInput.java
@@ -0,0 +1,18 @@
+package com.example.javaproject2.week1;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class JavaInput {
+    public void readTwoChars() throws IOException {
+        InputStreamReader is = new InputStreamReader(System.in);
+        int asciiCode1 = is.read();
+        int asciiCode2 = is.read();
+
+
+
+        System.out.println(asciiCode1);
+        System.out.println(asciiCode2);
+    }
+
+}
