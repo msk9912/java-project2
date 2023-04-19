318c79bddb4c1e3f5cfb5a7aa95d08b71c74f743
Ms Kim <myungseokim9912@gmail.com>
2023-04-19 15:31:20 +0900

    feat: Scanner - Scanner

--- src/main/java/com/example/javaproject2/week1/BufferedReaderEx.java
+++ src/main/java/com/example/javaproject2/week1/BufferedReaderEx.java
@@ -5,11 +5,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class BufferedReaderEx {
-    public void readALine() throws IOException {
+    public void plusTwoNumbers() throws IOException {
         InputStreamReader is = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(is); // 카트 가지고 오기
-
-        System.out.println("line1 = " + br.readLine());
-        System.out.println("line2 = " + br.readLine());
+        System.out.println(br.read() + br.read());
     }
 }
