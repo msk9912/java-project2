a3e7b274f09cd52af338e18e4f27b5f95ab468af
Ms Kim <myungseokim9912@gmail.com>
2023-04-24 17:58:27 +0900

    feat: week2 day1 -Codeup -변수타입

--- src/main/java/com/example/javaproject2/codeup/Codeup1029.java
+++ src/main/java/com/example/javaproject2/codeup/Codeup1029.java
@@ -2,10 +2,10 @@ package com.example.javaproject2.codeup;
 
 import java.util.Scanner;
 
-public class Codeup1028 {
+public class Codeup1029 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        long num = sc.nextLong();
-        System.out.println(num);
+        double num = sc.nextDouble();
+        System.out.printf("%.11f", num);
     }
 }
