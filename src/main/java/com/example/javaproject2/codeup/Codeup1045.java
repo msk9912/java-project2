3f0a1cbe6c7d110da8dc5779b0d2e704609a1042
Ms Kim <myungseokim9912@gmail.com>
2023-04-26 14:36:23 +0900

    feat: week2 day3 - codeup

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1045.java
@@ -0,0 +1,18 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1045 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] str = sc.nextLine().split(" ");
+        int a = Integer.parseInt(str[0]);
+        int b = Integer.parseInt(str[1]);
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+        System.out.printf("%.2f", a / (double)b);
+    }
+}
---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1046.java
@@ -0,0 +1,18 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1046 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] str = sc.nextLine().split(" ");
+        int a = Integer.parseInt(str[0]);
+        int b = Integer.parseInt(str[1]);
+        int c = Integer.parseInt(str[2]);
+
+        int sum = a + b + c;
+
+        System.out.println(sum);
+        System.out.printf("%.1f", sum/3.0);
+    }
+}
