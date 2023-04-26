f36df5cba90c0a597fb84b68869d748801612f61
Ms Kim <myungseokim9912@gmail.com>
2023-04-26 14:10:04 +0900

    feat: week2 day3 - remainder

--- src/main/java/com/example/javaproject2/week2/day3/ReminderQuote.java
+++ src/main/java/com/example/javaproject2/week2/day3/ReminderQuote.java
@@ -2,6 +2,15 @@ package com.example.javaproject2.week2.day3;
 
 public class ReminderQuote {
     public static void main(String[] args) {
+        int iVal = 1;
+        System.out.println(iVal / 2);
 
+        // 몫
+        int quote = 10 / 3;
+        //나머지
+        int remainder = 10 % 3;
+
+        System.out.println(quote);
+        System.out.println(remainder);
     }
 }
