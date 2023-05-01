d2e9f921c6694aa60a117f83aba6f3e04ba591d5
Ms Kim <myungseokim9912@gmail.com>
2023-05-01 14:11:12 +0900

    feat: week3 day1 - codeup - for문

---
+++ src/main/java/com/example/javaproject2/week3/LoopChar.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.week3;
+
+public class LoopChar {
+    public static void main(String[] args) {
+        for (char c = '가'; c <= '힣'; c++) {
+            System.out.printf("%c ", c);
+        }
+    }
+}
